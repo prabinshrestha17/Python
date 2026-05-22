c2a724b9f3ba3ecf44d1dda4e549c1aeccb5ea14
prabinshrestha17 <xthaprabin125@gmail.com>
2026-05-22 16:23:55 +0900

    feat: deep-learning

--- deeplearning/deeplearning.py
+++ deeplearning/deeplearning.py
@@ -63,4 +63,19 @@ len(test_labels)
 plt.figure()
 plt.imshow(train_images[0])
 plt.colorbar()
+plt.grid(True)
+plt.show()
+
+
+train_images = train_images /255.0
+test_images = test_images /255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap = plt.cm.binary)
+    plt.xlabel(classNames[train_labels[i]])
 plt.show()
